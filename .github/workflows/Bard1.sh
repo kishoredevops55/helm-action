69d436d471ba447152ce1c49d160e20a51295e8c
kishoredevops55 <96948636+kishoredevops55@users.noreply.github.com>
2024-01-23 23:06:56 +0530

    Update Bard1.sh

--- .github/workflows/Bard1.sh
+++ .github/workflows/Bard1.sh
@@ -7,13 +7,13 @@ keyVaultName="your-keyvault-name"
 secretNames=$(az keyvault secret list --vault-name $keyVaultName --query "[].name" -o tsv)
 
 # Create or clear the output file
-outputFile="secrets_output.txt"
-echo -n > "$outputFile"
+outputFile="secrets_output.csv"
+echo "SecretName,SecretValue" > "$outputFile"
 
 # Loop through secret names and get values
 for secretName in $secretNames; do
     secretValue=$(az keyvault secret show --vault-name $keyVaultName --name "$secretName" --query 'value' -o tsv)
-    echo -e "$secretName\t$secretValue" >> "$outputFile"
+    echo -e "\"$secretName\",\"$secretValue\"" >> "$outputFile"
 done
 
 echo "Secrets exported to $outputFile"
