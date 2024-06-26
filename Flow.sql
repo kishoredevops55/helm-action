518bcd0ad040cf526abb67aacdd316e9dadf2d27
kishoredevops55 <96948636+kishoredevops55@users.noreply.github.com>
2024-06-26 16:41:34 +0530

    Create Flow.sql

---
+++ Flow.sql
@@ -0,0 +1,26 @@
+Title: ACR Tags Reduction and Vulnerability Analysis Workflow
+
+[ACR Tags Reduction]
+|-- Active Development
+|-- Non-Active Development
+
+[Production Release Tag Checkpoint]
+|-- DevOps Checklist
+|-- CI Workflow Control
+
+[Python Script for Vulnerability Analysis]
+|-- Analyze Prisma Report
+|-- Read Excel File
+
+[Data Filtration]
+|-- 'ESOB' Substring
+|-- 'ESOBF' Substring
+
+[Tagging]
+|-- 'np' for Non-Production
+|-- 'rel' for Production
+|-- Count and Compare Tags
+
+[Weekly Calls]
+|-- Progress and Issues
+|-- Include POD PMs (Monthly)
